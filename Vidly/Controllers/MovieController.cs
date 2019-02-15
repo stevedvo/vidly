e162525bf69a554235b6b239766499472e30e0f2
Steve DeVeaux <steve.deveaux@kloc.co.uk>
2019-02-15 13:40:18 +0000

    - add custom routes

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -19,5 +19,31 @@ namespace Vidly.Controllers
 
 			return View(movie);
 		}
+
+		public ActionResult Edit(int id)
+		{
+			return Content("id = " + id);
+		}
+
+		public ActionResult Index(int? pageIndex, string sortBy)
+		{
+			if (!pageIndex.HasValue)
+			{
+				pageIndex = 1;
+			}
+
+			if (string.IsNullOrWhiteSpace(sortBy))
+			{
+				sortBy = "Name";
+			}
+
+			return Content(String.Format($"pageIndex={pageIndex}&sortBy={sortBy}"));
+		}
+
+		[Route("Movie/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+		public ActionResult ByReleaseDate(int year, int month)
+		{
+			return Content(year + "/" + month);
+		}
 	}
 }
