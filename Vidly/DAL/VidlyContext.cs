8948a53a3dcb54c820feedbd5559332db2be7eb0
Steve DeVeaux <steve.deveaux@kloc.co.uk>
2019-02-26 16:14:41 +0000

    - install EntityFramework package - add DBContext for Vidly - add initial migration - update Data Models with additional migrations - update Controllers & Views to retrieve & output additional info with eager loading

---
+++ Vidly/DAL/VidlyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DAL
+{
+	public class VidlyContext : DbContext
+	{
+		public DbSet<Movie> Movies { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+	}
+}
+
