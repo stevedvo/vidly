5ab6da70f4a0819cc5fc76c8fb683ae26c36b7c9
Steve DeVeaux <steve.deveaux@kloc.co.uk>
2019-03-19 14:12:18 +0000

    - add Rental entity - add NewRental DTO - add NewRentals API Controller - add migrations

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.DAL;
+using Vidly.DTOs;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+	public class NewRentalsController : ApiController
+	{
+		private VidlyContext _context = new VidlyContext();
+
+		public NewRentalsController()
+		{
+			_context = new VidlyContext();
+		}
+
+		// POST: /api/rentals
+		[HttpPost]
+		public IHttpActionResult CreateNewRentals(NewRentalDTO newRentalDto)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
