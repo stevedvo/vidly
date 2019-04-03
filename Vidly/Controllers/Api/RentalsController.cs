6c9cfe5d58e94a3f63c9ac454d86bb30d5dbe3a9
Steve DeVeaux <steve.deveaux@kloc.co.uk>
2019-04-03 13:25:48 +0100

    - create View to see all current Rentals - add methods to RentalsController and api/RentalsController to handle page and loading Rentals over AJAX for the DataTable - add link to new page in NavBar

--- Vidly/Controllers/Api/RentalsController.cs
+++ Vidly/Controllers/Api/RentalsController.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.DAL;
+using Vidly.DTOs;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
@@ -15,6 +19,45 @@ namespace Vidly.Controllers.Api
 		public RentalsController()
 		{
 			_context = new VidlyContext();
+		}
+
+		//GET /api/rentals
+		public IHttpActionResult GetRentals()
+		{
+			IList<RentalListDTO> rentalListDTOs = new List<RentalListDTO>();
+
+			var rentals = _context.Rentals.Include(r => r.Customer).Include(r => r.Movie).Where(r => r.DateReturned == null).ToList();
+
+			if (rentals.Count > 0)
+			{
+				foreach (var rental in rentals)
+				{
+					var rentalListCustomerDTO = new RentalListCustomerDTO
+					{
+						Id = rental.Customer.Id,
+						Name = rental.Customer.Name
+					};
+
+					var rentalListMovieDTO = new RentalListMovieDTO
+					{
+						Id = rental.Movie.Id,
+						Name = rental.Movie.Name
+					};
+
+					var rentalListDTO = new RentalListDTO
+					{
+						Id = rental.Id,
+						Customer = rentalListCustomerDTO,
+						Movie = rentalListMovieDTO,
+						DateRented = rental.DateRented,
+						DateRentedString = rental.DateRented.ToString("dd MMM yyyy")
+					};
+
+					rentalListDTOs.Add(rentalListDTO);
+				}
+			}
+
+			return Ok(rentalListDTOs);
 		}
 
 		//PUT /api/rentals/1
---
+++ Vidly/DTOs/RentalListDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+	public class RentalListDTO
+	{
+		public int Id { get; set; }
+		public RentalListCustomerDTO Customer { get; set; }
+		public RentalListMovieDTO Movie { get; set; }
+
+		public DateTime DateRented { get; set; }
+		public DateTime? DateReturned { get; set; }
+
+		public string DateRentedString { get; set; }
+	}
+}
